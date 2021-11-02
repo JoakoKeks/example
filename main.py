022ad85473fc1001bdddcfec943a9d081bad5d40
joaquin aravena <joaquin.aravena.e@gmail.com>
2021-11-02 17:02:04 -0300

    Octavo Commit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "Hello World!"
-
-if __name__=="__main__":
-    app.run(debug=True)
+from flask import Flask, render_template, session, flash, request, redirect
+app= Flask(__name__)
+@app.route('/')
+def index():
+  return "<h1>Welcome to CodingX</h1>"
