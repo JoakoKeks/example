e0fd1060733e01e9401f3e001223518b03f1bb98
joaquin aravena <joaquin.aravena.e@gmail.com>
2021-11-11 12:36:11 -0300

    Doceavo commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, session, flash, request, redirect
 app= Flask(__name__)
 @app.route('/')
+
 def index():
-  return "<h1>Welcome to CodingX</h1>"
+  return render_template("index.html")
+
+if __name__=="__main__":   # Asegúrese de que este archivo se ejecute directamente y no desde un módulo diferente
+    app.run(debug=True)    # ejecuta la aplicación en modo depuracion
